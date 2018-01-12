4bff6077722610eb1e4157ab7181b86f7bd6a3d4
binay1108 <binay1108@yahoo.com>
2018-01-12 12:17:44 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable modname {}
+
+variable SubnetId {
+	default = "subnet-5357271a"
+	description = "The subnet id of a default vpc in the working region"
+}
+
+variable SecGroupId {
+	description = "this variable doesn't have a default value but it is passed from the main code"
+}
